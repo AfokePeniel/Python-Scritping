681161e530fba1be5a14733a28424a1e64487642
afokepeniel <afokekowho@gmail.com>
2024-11-04 14:21:48 -0500

    temp cal code

---
+++ Calculate-Temperature.py
@@ -0,0 +1,16 @@
+"""
+This is a program that calculates temperature.
+It askes the user to input details for temperature conversion.
+From Celsius to Fahrenheit and Fahrenheit to Celsius.
+
+"""
+
+#Calculate Temperature
+
+celsius = int(input("Enter a value for celcius: ")) 
+fahrenheit = int(input("Enter a value for fahrenheit: "))
+cel_to_fah = round((fahrenheit- 32) * 5/9)
+fah_to_cel = round((celsius * 9/5) + 32)
+
+print(f"Your temperature in Celsius is: {fah_to_cel}""°C")
+print(f"Your temperature in Fahrenheit is: {cel_to_fah}""°F")
