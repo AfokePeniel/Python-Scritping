64747b877562351f55eacdf3f6b7d05faa09958a
afokepeniel <afokekowho@gmail.com>
2024-11-11 16:07:21 -0500

    code modify

--- Rollercoster-Ride.py
+++ Rollercoster-Ride.py
@@ -1,5 +1,5 @@
 """
-This ia a program on the selection of certain conditions
+This is a program on the selection of certain conditions
 before a user can go or qualify for a Roller Coaster ride.
 
 """
@@ -20,9 +20,12 @@ if height >= 120:
     elif age <=18:
         bill = 7
         print("Youth tickets are $7")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("Your ticket is $0")
     else:
         bill = 12
-        print("Adult tickets are $12") 
+        print("Adult tickets are $12")    
     picture = (input("Do you want a photo while rollercoasting? Enter 'Y' or 'y' for Yes and 'N' or 'n' for No: "))
     if picture == 'Y' :
         bill += 3
